34cb4d0ea8c2c0f7399791673995cd170d73ad29
bahgep <wonderlboson@gmail.com>
2021-05-25 17:40:59 +0800

    second commint

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function people() {
+        return "Interviewer: 汪德彥 ";
+    }
+    //
+}
